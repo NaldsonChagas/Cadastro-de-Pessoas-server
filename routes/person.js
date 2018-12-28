e4da433686555b982b2958658f6971b87fe65e56
Naldson Chagas <naldson@protonmail.com>
2018-12-28 12:08:32 -0300

    removing persons

--- routes/person.js
+++ routes/person.js
@@ -11,21 +11,21 @@ const router = express.Router();
 router.get('/', (req, res) => {
   db.find({}).sort({ name: 1 }).exec((err, persons) => {
     if (err) res.status(400).json({ error: err });
-    else res.status(200).json({ users: persons });
+    else res.status(200).json(persons);
   });
 });
 
 router.post('/', (req, res) => {
   db.insert(req.body, (err, person) => {
     if (err) res.status(400).json({ error: err });
-    else res.status(200).json({ person: person });
+    else res.status(200).json({ id: person._id });
   });
 });
 
 router.get('/:id', (req, res) => {
   db.findOne({ _id: req.params.id }, (err, person) => {
-    if (err) res.status(400).json({error: err});
-    else res.status(200).json({person: person});
+    if (err) res.status(400).json({ error: err });
+    else res.status(200).json({ person: person });
   });
 });
 
@@ -34,7 +34,10 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-
+  db.remove({ _id: req.params.id }, {}, (err, numRemoved) => {
+    if (err) res.status(400).json({ error: err });
+    else res.status(200).json({ message: 'ok' });
+  });
 });
 
 module.exports = router;
