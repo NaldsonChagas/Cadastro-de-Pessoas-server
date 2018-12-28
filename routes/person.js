b9bbc815c22726d6be7f2d1a78da6c38cecd845e
Naldson Chagas <naldson@protonmail.com>
2018-12-28 12:32:39 -0300

    update parameter

--- routes/person.js
+++ routes/person.js
@@ -31,7 +31,7 @@ router.get('/:id', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-  db.update({ _id: req.params.id }, null, (err, numUpdate) => {
+  db.update({ _id: req.params.id }, req.body, (err, numUpdate) => {
     if (err) error(res, 400, err);
     else res.status(200).json({ numUpdate: numUpdate });
   });
