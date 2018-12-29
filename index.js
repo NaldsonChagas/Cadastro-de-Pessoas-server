3b3bf5e2061e7a59e9e3468878f3f25b07c56bee
Naldson Chagas <naldson@protonmail.com>
2018-12-29 05:01:36 -0300

    cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const person = require('./routes/person');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors({origin: '*'}));
 
 app.use('/person', person);
 
